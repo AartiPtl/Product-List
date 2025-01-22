60942df0534b30bad9852133202321cbb59e45f1
Aartiptl <aartipatle777@gmail.com>
2025-01-22 16:48:08 +0530

    update

--- App.js
+++ App.js
@@ -1,11 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
 
-export default function App() {
+const App = () => {
+
+  const [data, setData] = useState([]);
+  const searchUser = async (text) => {
+    const url = 'https://jsonplaceholder.typicode.com/posts/1';
+    console.warn(url);
+    let result = await fetch(url);
+    result = await result.json();
+    console.log(text)
+    if (result) {
+      setData(result)
+    }
+    //useEffect(()=> {
+    //searchUser();
+    //},[])
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        placeholder={'Search'}
+        onChangeText={(text) => searchUser(text)}
+      />
+      {
+        data.length ?
+          data.map((item) => <View
+            style={{ padding: 10, flexDirection: 'row', justifyContent: 'space-between' }}>
+            <Text style={{ fontSize: 20 }}>{item.name}</Text>
+            <Text style={{ fontSize: 20 }}>{item.age}</Text>
+            <Text style={{ fontSize: 20 }}>{item.email}</Text>
+          </View>)
+          : null
+      }
     </View>
   );
 }
@@ -17,4 +47,12 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input: {
+    borderColor: "blue",
+    borderWidth: 1,
+    fontSize: 20,
+    padding: 10
+  }
 });
+
+export default App;
